bd741aad0bc175e81c3a330f51144c77dc5bed5d
akquynh <akquynh3@gmail.com>
2024-09-29 08:22:22 -0700

    edited code

--- js/01-larger-or-smaller.js
+++ js/01-larger-or-smaller.js
@@ -4,7 +4,7 @@ let value02;
 
 // Create function to compare the numbers
 function largerOrSmaller() {
-    
+
     // Get input from user
     value01 = parse.Int(prompt('Please enter a number:'));
     value02 = parse.Int(prompt('Please enter a second number:'));
@@ -18,4 +18,4 @@ function largerOrSmaller() {
         document.body.innerHTML = "Both numbers are equal.";
     }
 }
-compareNumbers();
+largerOrSmaller();
